588c3178fee5d3623f5a8e478e8128c1f2fe9f98
Maritsa28 <ggjob1@gmail.com>
2024-05-20 23:38:26 +0400

    first commit

---
+++ level_01/homework/homework1.py
@@ -0,0 +1,13 @@
+name1 = "I am "
+name = "levan"
+surname = "maritsashvili"
+born = " I was born in Dusheti city"
+education = "I am currently studying in Goa and I am very happy"
+age1 = "year"
+height1 = "height"
+age = 27 
+height = 185.5
+
+
+print(name1 + name + " " + surname + " " + str(age) + " " + age1 + " " + height1 +" "+ str(height) + " " + education)
+
